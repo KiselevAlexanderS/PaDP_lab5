7df4dc160b00a9edd9bc7025fdce2ee885e59603
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-19 04:16:51 +0300

    Scripted auto-commit on change (2022-02-19 04:16:50) by gitwatch.sh

--- src/main/java/GetResult.java
+++ src/main/java/GetResult.java
@@ -1,3 +1,17 @@
 public class GetResult {
     private String url;
+    private int count;
+
+    public GetResult(String url, int count) {
+        this.count = count;
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
