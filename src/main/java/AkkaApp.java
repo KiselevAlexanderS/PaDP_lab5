0157977b3c14a7b45a774154377a8cadebda1acd
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-19 04:03:54 +0300

    Scripted auto-commit on change (2022-02-19 04:03:54) by gitwatch.sh

--- src/main/java/AkkaApp.java
+++ src/main/java/AkkaApp.java
@@ -1,11 +1,19 @@
+import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
 
 public class AkkaApp {
     public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create("routes");
-        final Http http
+        final Http http = Http.get(system);
+        final ActorMaterializer materia = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <>;
+        final
     }
 }
