2a8f46055fe85cb80fd57cccf8acebb61eb831bb
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-19 04:09:15 +0300

    Scripted auto-commit on change (2022-02-19 04:09:15) by gitwatch.sh

--- src/main/java/AkkaApp.java
+++ src/main/java/AkkaApp.java
@@ -1,12 +1,15 @@
 import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 public class AkkaApp {
     public static void main(String[] args) throws IOException {
@@ -14,6 +17,14 @@ public class AkkaApp {
         final Http http = Http.get(system);
         final ActorMaterializer materia = ActorMaterializer.create(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <>;
-        final
+        final CompletionStage<ServerBinding> bind = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materia
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        bind.thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 }
