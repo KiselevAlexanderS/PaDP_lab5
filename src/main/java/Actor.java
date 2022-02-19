18c7ee970478bbb4bbcd353bb94e43e68a11ba15
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-19 04:12:14 +0300

    Scripted auto-commit on change (2022-02-19 04:12:14) by gitwatch.sh

--- src/main/java/Actor.java
+++ src/main/java/Actor.java
@@ -1,7 +1,7 @@
 import akka.actor.AbstractActor;
 
 import java.util.Map;
-import java.util.Map;
+import java.util.HashMap;
 
 public class Actor extends AbstractActor {
     private Map<String, Float> storage = new HashMap<>();
