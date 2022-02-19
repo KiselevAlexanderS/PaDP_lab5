d9051ce9754654dc71f94b4ea68b8b701f2b8561
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-19 04:02:06 +0300

    Scripted auto-commit on change (2022-02-19 04:02:06) by gitwatch.sh

--- src/main/java/AkkaApp.java
+++ src/main/java/AkkaApp.java
@@ -1,9 +1,11 @@
 import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
 
 import java.io.IOException;
 
 public class AkkaApp {
-    public static main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create("routes");
+        final Http http
     }
 }
